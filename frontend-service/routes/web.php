5d0248e51c577d2f4d0d6d56c93fc100a6cd67ae
DC <dariocapdevila@gmail.com>
2024-10-02 02:12:42 -0300

    Close

--- frontend-service/routes/web.php
+++ frontend-service/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return '1';
 });
+
+Route::get('/a', function () {
+    return '1a';
+});
--- kitchen-service/routes/web.php
+++ kitchen-service/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return '2';
 });
+
+Route::get('/a', function () {
+    return '2a';
+});
